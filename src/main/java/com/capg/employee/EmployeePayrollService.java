77152cc00c6c00469b9cfb7bf3f18bf29b35dfbc
jindalakhil <akhiljindal222@gmail.com>
2020-10-20 23:34:08 +0530

    [Akhil]Added: Read and write employee payroll data to console

---
+++ src/main/java/com/capg/employee/EmployeePayrollService.java
@@ -0,0 +1,40 @@
+package com.capg.employee;
+
+import java.util.ArrayList;
+import java.util.*;
+
+public class EmployeePayrollService {
+	private List<EmployeePayrollData> empList;
+
+	public EmployeePayrollService() {
+	}
+
+	public EmployeePayrollService(List<EmployeePayrollData> empList) {
+		this.empList = empList;
+	}
+
+	private void readEmployeePayrollData(Scanner s) {
+		System.out.println("Enter ID");
+		int id = s.nextInt();
+		System.out.println("Enter Name");
+		String name = s.next();
+		System.out.println("Enter salary");
+		Double salary = s.nextDouble();
+		empList.add(new EmployeePayrollData(id, name, salary));
+	}
+
+	private void writeEmployeePayrollData(Scanner consoleInputReader) {
+		System.out.println("\nWriting Employee Payroll Data to Console\n" + empList);
+
+	}
+
+	public static void main(String[] args) {
+		ArrayList<EmployeePayrollData> empList = new ArrayList<>();
+		EmployeePayrollService empService = new EmployeePayrollService(empList);
+		Scanner consoleInputReader = new Scanner(System.in);
+		empService.readEmployeePayrollData(consoleInputReader);
+		empService.writeEmployeePayrollData(consoleInputReader);
+
+	}
+
+}
